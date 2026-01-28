460f2a69f5713871024dea2a2c20557e02dc44df
Chetan Prajapat <chetanprajapat8800@gmail.com>
2026-01-29 02:06:41 +0530

    Add files via upload

---
+++ sumTw.py
@@ -0,0 +1,26 @@
+a = input("enter a :")
+b =input("enter b :")
+
+avg = a + b
+print(avg)
+
+
+
+# it to be a string, change to be a ype casst
+
+
+
+a = int(input("enter a :"))
+b = int(input("enter b :"))
+
+avg = a + b
+print(avg)
+
+# ptrinr the avagerva 
+# 
+
+a = int(input("enter a :"))
+b = int(input("enter b :"))
+
+avg = (a + b)/2
+print("avg", avg)
